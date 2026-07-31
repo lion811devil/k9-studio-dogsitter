522714801f39ca312e3115eccd4c95c5b7544d11
lion811devil <lion.811.devil.gn@gmail.com>
2026-07-31 09:43:03 +0200

    Add files via upload
    Initial release K9 Studio Dogsitter

---
+++ config.js
@@ -0,0 +1,7 @@
+window.K9_CONFIG = Object.freeze({
+  SUPABASE_URL: 'https://elkxxdumdiifemqummrz.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_IZ07LxeT7czu1AeY8_yZhw_v_cINFie',
+
+  APP_NAME: 'K9 Studio Dogsitter',
+  ORGANIZATION_NAME: 'K9 Napoletano Academy'
+});
---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE='k9-studio-dogsitter-v1.0.0';
+const STATIC=['./','./index.html','./styles.css','./config.js','./app.js','./manifest.webmanifest','./assets/logo.png','./assets/icon-192.png','./assets/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(e.request.method!=='GET'||u.origin!==self.location.origin)return;
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{if(r.ok&&r.type==='basic'){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return r;}).catch(()=>caches.match('./index.html'))));
+});
