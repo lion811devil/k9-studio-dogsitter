7a5341a487fc041cd3a24b7f2e28ee14ec0fa05b
lion811devil <lion.811.devil.gn@gmail.com>
2026-08-13 12:43:47 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-// K9 Studio Dogsitter 12.8.7 — Service Worker stabile
-const K9_SW_VERSION='12.8.7';
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',()=>{});
